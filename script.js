d41a6b1826fb1de4cac295da92634038af2a15b1
RidersWeb <samara_expert@mail.ru>
2021-10-22 22:10:08 +0400

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+let money = prompt("Ваш бюджет на месяц", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+const appData = {
+    budget : money,
+    timeData : time,
+}
---
+++ test.js
@@ -0,0 +1,27 @@
+let a = 3;
+
+function addTwo(x) {
+    let result = x + 2;
+    return result;
+}
+
+let b = addTwo(a);
+console.log(b);
+
+
+var number = 6;
+var string = "Hello world";
+var sym =  "symbol";
+var boolean = "True False";
+var obj = {};
+
+// null =  не существует к примеру переменной;
+// undefined = нет значения в переменной;
+
+let person = {
+    name: "Slava",
+    age: 37,
+    isMarried: true
+}
+
+console.log(person.name)
